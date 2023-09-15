dee6f8e432fcb34c91812ed22e28e8c31b637238
DonCorrupto <georgealzate18@hotmail.com>
2023-09-14 23:01:34 -0500

    Pagina Principal (Bug)

---
+++ Frontend/web/components/fondo_video.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <video autoplay muted loop>
+        <source src="@/src/assets/video_pavimento.mp4" type="video/mp4">
+        Tu navegador no admite la reproducción de videos.
+      </video>
+    </div>
+  </template>
+  
+  <script>
+  </script>
+  
--- Frontend/web/pages/index.vue
+++ Frontend/web/pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div style="position: relative;">
+      <fondo-video />
+      <div style="display: flex; justify-content: center; align-items: center; min-height: 100vh; border: 2px solid red;">
+        <div style="text-align: center;">
+          <h2>Automatiza tus Retro-Calculos con nuestra herramientas para que tus pavimentos queden como los de Chile</h2>
+          <br>
+          <b-button pill variant="warning">Ingrese tu archivo</b-button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import fondo_video from '../components/fondo_video.vue';
 export default {
-  name: 'IndexPage'
-}
+  components: { fondo_video },
+  data() {
+    return {
+      videoURL: 'https://www.youtube.com/clip/UgkxQD_tEI6PzfTMQtWvJYN1zRD0LJNuDPvO.mp4',
+    };
+  },
+};
 </script>
+
+
+
+
+
