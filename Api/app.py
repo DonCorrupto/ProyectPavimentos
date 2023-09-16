354a5df2495254fd5495ea97efacf54c44b97a47
DonCorrupto <georgealzate18@hotmail.com>
2023-09-15 22:02:01 -0500

    Obtener el csv del usuario

--- Api/app.py
+++ Api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,6 +9,22 @@ CORS(app)
 def get_data():
     mensaje = {'mensaje': 'Hola desde tu API Flask'}
     return jsonify(mensaje)
+
+@app.route('/api/cargar_csv', methods=['POST'])
+
+def cargar_csv():
+    archivo_csv = request.files['archivo_csv']
+    
+    # Procesa el archivo CSV (ejemplo con la biblioteca csv de Python)
+    import csv
+    datos_csv = []
+    for linea in archivo_csv:
+        fila = linea.decode().strip().split(',')
+        datos_csv.append(fila)
+    
+    # Realiza acciones con los datos del CSV aquí
+    
+    return 'Archivo CSV cargado exitosamente.'
 
 
 if __name__ == '__main__':
--- Frontend/web/pages/index.vue
+++ Frontend/web/pages/index.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    <div style="position: relative;">
-      <fondo-video />
-      <div style="display: flex; justify-content: center; align-items: center; min-height: 100vh; border: 2px solid red;">
-        <div style="text-align: center;">
-          <h2>Automatiza tus Retro-Calculos con nuestra herramientas para que tus pavimentos queden como los de Chile</h2>
-          <br>
-          <b-button pill variant="warning">Ingrese tu archivo</b-button>
+    <div
+      style="display: flex; justify-content: center; align-items: center; min-height: 100vh; background-image: url('https://excavacionesgrasa.com/wp-content/uploads/2022/02/Pavimentos-rigidos-y-pavimentos-flexibles.jpg'); background-size: cover; background-position: center;">
+      <div style="text-align: center;">
+        <h2 style="color: white;">Automatiza tus Retro-Calculos con nuestra herramientas para que tus pavimentos queden
+          como los de Chile</h2>
+        <br>
+        <div>
+          <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')" pill variant="warning">Ingrese tu
+            archivo</b-button>
+
+          <b-modal id="bv-modal-example" hide-footer>
+            <template #modal-title>
+              Formato CSV
+            </template>
+            <form @submit.prevent="handleFileUpload">
+              <input type="file" ref="fileInput" accept=".csv">
+              <b-button style="margin-left: 5%;" v-b-modal.modal-prevent-closing pill variant="info" type="submit">Cargar CSV</b-button>
+              <br>
+              <br>
+              <b-button  block @click="$bvModal.hide('bv-modal-example')" pill variant="danger">Cerrar</b-button>
+            </form>
+          </b-modal>
         </div>
       </div>
     </div>
@@ -14,16 +29,44 @@
 </template>
 
 <script>
-import fondo_video from '../components/fondo_video.vue';
+import axios from 'axios';
 export default {
-  components: { fondo_video },
-  data() {
-    return {
-      videoURL: 'https://www.youtube.com/clip/UgkxQD_tEI6PzfTMQtWvJYN1zRD0LJNuDPvO.mp4',
-    };
-  },
-};
+  methods: {
+    async handleFileUpload() {
+      const fileInput = this.$refs.fileInput;
+      const file = fileInput.files[0]; // Obtiene el primer archivo seleccionado
+
+      if (file) {
+        // Verifica que se haya seleccionado un archivo
+        if (file.type === 'text/csv') {
+          // Verifica que sea un archivo CSV
+          // Ahora puedes trabajar con el archivo CSV, por ejemplo, leer su contenido
+          const formData = new FormData();
+          formData.append('archivo_csv', file);
+
+          try {
+            // Realiza la solicitud POST para enviar el archivo CSV a la API Flask
+            await axios.post('http://127.0.0.1:5000/api/cargar_csv', formData, {
+              headers: {
+                'Content-Type': 'multipart/form-data'
+              }
+            });
+            alert('Archivo CSV enviado correctamente.');
+          } catch (error) {
+            console.error('Error al enviar el archivo CSV:', error);
+            alert('Error al enviar el archivo CSV.');
+          }
+        } else {
+          alert('Selecciona un archivo CSV válido.');
+        }
+      } else {
+        this.$swal('Selecciona un archivo antes de cargarlo.');
+      }
+    },
+  }
+}
 </script>
+
 
 
 
